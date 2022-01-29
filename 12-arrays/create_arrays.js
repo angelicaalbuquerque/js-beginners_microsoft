53d9a845d38748b6dc71d21f428906e6c67b4b3e
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-01-29 18:22:16 -0300

    Criando Arrays

---
+++ 12-arrays/create_arrays.js
@@ -0,0 +1,18 @@
+//Criando array
+let animais = ["Cachorro", "Gato", "Girafa", "Cavalo"];
+console.log(animais);
+
+let idiomas = new Array("Inglês", "Português", "Espanhol", "Francês");
+console.log(idiomas);
+
+//Acessar elemento de um Array
+let ingles = idiomas[0];
+console.log(ingles);
+
+//Medindo tamanho de um array
+let frutas = ["Banana", "Pêra", "Laranja", "Abacaxi"];
+console.log(frutas.length);
+
+//Acessar o último elemento
+let ultimaFruta = frutas[frutas.length - 1];
+console.log(ultimaFruta);
