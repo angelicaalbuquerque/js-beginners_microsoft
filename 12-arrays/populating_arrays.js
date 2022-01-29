a9d4dd0653eba455e15efe4b59bc8d31d190c905
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-01-29 18:42:58 -0300

    Populando Arrays

--- 12-arrays/populating_arrays.js
+++ 12-arrays/populating_arrays.js
@@ -27,3 +27,33 @@ console.log(arr3[2]); //Last index of array
 console.log(arr3[arr3.length - 1]); //Index: 2
 console.log(arr3[0]); //No value yet
 console.log(arr3[1]); //No value yet
+
+//Array index
+console.log(Object.keys(arr1)); //Acessa diretamente a chave do objeto
+console.log(Object.values(arr1)); //Acessa diretamente o valor do objeto
+
+//Adding data after creation
+console.log("\n********Adding data after creation Part 2********\n");
+let arr4 = ["Angelica", 31, true];
+arr4.push("Rio de Janeiro", "Ciência da Computação");
+console.log(arr4);
+
+//with SPREAD OPERATOR
+console.log("\n********SPREAD OPERATOR********\n");
+let arr5 = ["A", "B"];
+let arr6 = [...arr5, "C", "D"]; //mantém o estado original e adiciona os três termos
+console.log(arr6);
+
+console.log("\n********Concatenando arrays********\n");
+let numbers = [1, 2];
+let moreNumbers = [3, 4];
+let allNumbers = [...numers, ...moreNumbers]; //para não sair uma lista dentro da outra, utilizo spread operator em moreNumbers
+console.log(allNumbers);
+
+console.log(
+  "\n********Concatenando arrays com push e spread operator********\n",
+);
+let data = ["Angélica", "carioca", 31];
+let moreData = ["Desenvolvedora", "Front-End", true];
+data.push(...moreData);
+console.log(data);
